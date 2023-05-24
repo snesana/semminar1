4b2d7f6931b14bc3da1cad90a558264d82d486ad
Your Snezhana <volhonskaya_snez@mail.ru>
2023-05-24 21:31:51 +0400

    Задача 6

--- seminar1/Program.cs
+++ seminar1/Program.cs
@@ -1,63 +1,65 @@
-﻿// Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+﻿// // Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое 
+// число большее, а какое меньшее.
 
-// a = 5; b = 7 -> max = 7
-// a = 2 b = 10 -> max = 10
-// a = -9 b = -3 -> max = -3
+// // a = 5; b = 7 -> max = 7
+// // a = 2 b = 10 -> max = 10
+// // a = -9 b = -3 -> max = -3
 
-int max = 0;
-int min = 0;
+// int max = 0;
+// int min = 0;
 
-Console.Write("Введите первое число: ");
-int firstNumber = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите первое число: ");
+// int firstNumber = Convert.ToInt32(Console.ReadLine());
 
-Console.Write("Введите второе число: ");
-int secondNumber = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите второе число: ");
+// int secondNumber = Convert.ToInt32(Console.ReadLine());
 
-if(firstNumber > secondNumber)
-{
-    max = firstNumber;
-    min = secondNumber;
-}
-else
-{
-    max = secondNumber;
-    min = firstNumber;
-}
-Console.WriteLine("max = " + max + " и " + "min = " + min);
-
-
+// if(firstNumber > secondNumber)
+// {
+//     max = firstNumber;
+//     min = secondNumber;
+// }
+// else
+// {
+//     max = secondNumber;
+//     min = firstNumber;
+// }
+// Console.WriteLine("max = " + max + " и " + "min = " + min);
 
 
-// Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
 
+
+
+// Задача 4: Напишите программу, которая принимает на вход три числа 
+// и выдаёт максимальное из этих чисел.
 // 2, 3, 7 -> 7
 // 44 5 78 -> 78
 // 22 3 9 -> 22
-int max = 0;
+// int max = 0;
 
-Console.Write("Введите первое число: ");
-int firstNumber = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите первое число: ");
+// int firstNumber = Convert.ToInt32(Console.ReadLine());
 
-Console.Write("Введите второе число: ");
-int secondNumber = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите второе число: ");
+// int secondNumber = Convert.ToInt32(Console.ReadLine());
 
-Console.Write("Введите третье число: ");
-int thirdNumber = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите третье число: ");
+// int thirdNumber = Convert.ToInt32(Console.ReadLine());
 
-if(firstNumber > max)
-{
-    max = firstNumber;
-}
-if(secondNumber > max)
-{
-    max = secondNumber;
-}
-if(thirdNumber > max)
-{
-    max = thirdNumber;
-}
+// if(firstNumber > max)
+// {
+//     max = firstNumber;
+// }
+// if(secondNumber > max)
+// {
+//     max = secondNumber;
+// }
+// if(thirdNumber > max)
+// {
+//     max = thirdNumber;
+// }
 
-Console.WriteLine("max = " + max);
+// Console.WriteLine("max = " + max);
 
 
 // Задача 6: Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
@@ -65,11 +67,37 @@ Console.WriteLine("max = " + max);
 // 4 -> да
 // -3 -> нет
 // 7 -> нет
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int remDiv = number % 2;
+
+if(remDiv == 0)
+{
+    Console.WriteLine("Да");
+}
+else
+{
+    Console.WriteLine("Нет");
+}
 
 // Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
 
 // 5 -> 2, 4
 // 8 -> 2, 4, 6, 8
+// Console.Write("Введите число: ");
+// int number = Convert.ToInt32(Console.ReadLine());
+
+// int evenNumber = 2;
+
+// if(number > 1)
+// {
+//     while(evenNumber <= number)
+//     {
+//         Console.Write(evenNumber + " ");
+//         evenNumber = evenNumber + 2;
+//     }
+// }
 
 // Задача 7 HARD по желанию - идет за 2 обязательных Напишите программу, которая принимает на вход целое число любой разрядности и на выходе показывает третью цифру слева этого числа или говорит, что такой цифры нет. Через строку решать нельзя.
 // 456111 -> 6
